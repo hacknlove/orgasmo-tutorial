215bb0fa47f49882b40fd2a88fb775c550392f27
hacknlove <antonio@hacknlove.org>
2022-10-21 09:47:05 +0200

    5-styling

--- components/CardLink.dynamic.jsx
+++ components/CardLink.dynamic.jsx
@@ -1,7 +1,7 @@
 export default function CardLink({ href, title }) {
     return (
       <div>
-        <a href={href} className="card">
+        <a href={href} className="card link">
           <h2>{title} &rarr;</h2>
         </a>
       </div>
